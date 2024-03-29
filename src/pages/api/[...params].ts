87765846307cf8181eb0773fac05627a7c3bcf36
Sheraz Ahmed <hi@isheraz.com>
2024-03-30 01:28:27 +0500

    updated api fn

--- src/pages/api/[...params].ts
+++ src/pages/api/[...params].ts
@@ -3,12 +3,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import lighthouse from 'lighthouse';
 import * as ChromeLauncher from 'chrome-launcher';
+import Cors from 'cors';
 
-function setCorsHeaders(res: NextApiResponse) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-}
+
 
 async function runLighthouse(url: string): Promise<string> {
 
@@ -23,31 +20,61 @@ async function runLighthouse(url: string): Promise<string> {
   return result;
 }
 
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST'], // Adjust the methods as needed
+  origin: '*', // Adjust the origin as needed
+});
+
+type MiddlewareFn = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: (err?: any) => void
+) => void;
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: MiddlewareFn): Promise<any> {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result?: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Set CORS headers
-  setCorsHeaders(res);
-
-  // Handle preflight requests for CORS
-  if (req.method === 'OPTIONS') {
-    return res.status(200).send('OK');
-  }
+  await runMiddleware(req, res, cors);
 
   const { params } = req.query;
+  console.log("🚀 ~ handler ~ params:", params)
 
-  if (params instanceof Array && params[0] === 'audit' && req.method === 'POST') {
-    const { url } = req.body;
-    if (!url || typeof url !== 'string') {
-      return res.status(400).json({ error: 'URL is required and must be a string' });
-    }
-
-    try {
-      const reportHtml = await runLighthouse(url);
-      res.status(200).send(reportHtml);
-    } catch (error) {
-      res.status(500).json({ error: error instanceof Error ? error.message : 'An error occurred' });
-    }
-  } else {
+  if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+
+  if (!Array.isArray(params)) {
+    return res.status(500).json({ error: 'Function not found' });
+  }
+
+  switch (params[0]) {
+    case 'audit':
+      const { url } = req.body;
+
+      if (!url || typeof url !== 'string') {
+        return res.status(400).json({ error: 'URL is required and must be a string' });
+      }
+
+      try {
+        const reportHtml = await runLighthouse(url);
+        res.status(200).send(reportHtml);
+      } catch (error) {
+        res.status(500).json({ error: error instanceof Error ? error.message : 'An error occurred' });
+      }
+      break;
+    default:
+      return res.status(500).json({ error: 'Function not found' });
+  }
+
 }
