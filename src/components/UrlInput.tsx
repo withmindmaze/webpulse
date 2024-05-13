c24e4b04a2a3ca7cc9181e8d96d33a721e7c72e0
Sheraz Ahmad <hi@isheraz.com>
2024-05-13 18:44:47 +0500

    iframe header colour for accessibility

--- src/components/UrlInput.tsx
+++ src/components/UrlInput.tsx
@@ -162,6 +162,12 @@ function UrlInput() {
             // @ts-ignore
             iframeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
         }
+        const names_labels = iframe.contentWindow.document.querySelector('.lh-audit-group__header') as HTMLElement;
+        // const names_labels = iframe.contentWindow.document.querySelector('.lh-audit__header lh-expandable-details__summary') as HTMLElement;
+        if (names_labels) {
+            names_labels.style.backgroundColor = '#3bbed9';
+            names_labels.style.color = 'white';
+        }
     };
 
 
