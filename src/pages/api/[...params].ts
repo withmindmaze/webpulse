58d3a46611abcaa6f47f308b083a723b65e5d173
Sheraz Ahmed <hi@isheraz.com>
2024-05-27 05:05:57 +0500

    Setup for Sentry loggings and errors

--- src/pages/api/[...params].ts
+++ src/pages/api/[...params].ts
@@ -3,6 +3,8 @@ import lighthouse, { OutputMode, Config } from 'lighthouse';
 import * as ChromeLauncher from 'chrome-launcher';
 import nextCors from 'nextjs-cors';
 import supabase from '@/utils/supabaseClient';
+import {wrapApiHandlerWithSentry, setContext, captureSession, captureMessage, captureException} from '@sentry/nextjs';
+
 import fs from 'fs';
 import path from 'path';
 
@@ -88,7 +90,7 @@ async function uploadReportFile(userId: any, url: any, htmlContent: any) {
   return filePath;
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler= async (req: NextApiRequest, res: NextApiResponse) => {
   // Initialize CORS middleware
   await nextCors(req, res, {
     methods: ['POST', 'OPTIONS'],
@@ -109,6 +111,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   console.log("🚀 ~ handler ~ params[0:", params[0]);
   console.log("🚀 ~ handler ~ req.body:", req.body)
+  setContext('report context', {request: req.body, params: params});
+  captureMessage('request', req.body);
+  
   switch (params[0]) {
     case 'audit':
       const { url, categories, device, user_id, generatedBy } = req.body;
@@ -143,6 +148,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
         res.status(200).json({ data: runnerResult, jsonReport: jsonReport });
       } catch (error) {
+        captureException(error);
         res.status(500).json({ error: error instanceof Error ? error.message : 'An error occurred' });
       }
       break;
@@ -151,3 +157,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
 }
+export default wrapApiHandlerWithSentry(handler, '/api/');
